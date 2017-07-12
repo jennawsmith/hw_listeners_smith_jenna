63d5f49a3538425919c42e568b64e1443018c3ea
Jenna-Smith <jennawsmith@outlook.com>
2017-07-11 21:01:58 -0400

    finish function to add new p when h1 is clicked

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var heading = document.getElementById('heading');
 
 heading.addEventListener('click', myFunction);
 
-function myFunction () {
+function myFunction() {
     var paragraph = document.createElement('p');
+    paragraph.innerHTML = 'This is click number: ';
+    heading.appendChild(paragraph);
 }
