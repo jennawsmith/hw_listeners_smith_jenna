031bdecab827a3692fc1c61e068ad0b00853e5d4
Jenna-Smith <jennawsmith@outlook.com>
2017-07-11 21:19:08 -0400

    add var numba to increment on click

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+var numba = 0
+
 var heading = document.getElementById('heading');
 
 heading.addEventListener('click', myFunction);
 
 function myFunction() {
     var paragraph = document.createElement('p');
-    paragraph.innerHTML = 'This is click number: ';
+    paragraph.innerHTML = 'This is click number: ' + ++numba;
     document.body.appendChild(paragraph);
 }
