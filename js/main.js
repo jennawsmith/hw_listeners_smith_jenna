a3bf3798f1f790ba4c3698a1588f8cf401907e68
Jenna-Smith <jennawsmith@outlook.com>
2017-07-13 13:39:40 -0400

    add variables for each form field

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var firstName = document.getElementsByTagName('input')[0];
+
+var lastName = document.getElementsByTagName('input')[1];
+
+var email = document.getElementsByTagName('input')[2];
+
+var message = document.getElementsByTagName('textarea')[0];
