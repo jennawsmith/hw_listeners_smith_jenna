d1ef3277471b2766814ee572bc0e48318886a30b
Jenna-Smith <jennawsmith@outlook.com>
2017-07-13 17:45:43 -0400

    added hover js event listener (color not working yet)

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//This listener will change the paragraph text to "Oops, I can't find the text!" when clicked
+
+//This listener will change h1 text color when hovered and then back when not hovered
+var hover = document.getElementById('hoverme');
+
+hover.addEventListener('mouseenter', function (event) {
+    event.target.style.color = 'white';
+});
+
+hover.addEventListener('mouseout', function (event) {
+    event.target.style.color = '#6d7993';
+});
+
+
+//This listener will change the background-color of the sections when a key is pressed
+
+var changeColor = document.getElementsByClassName('changeColor');
+
+changeColor.addEventListener('keydown', function (event1) {
+    event1.body.style.color = 'black';
+    return false;
+});
