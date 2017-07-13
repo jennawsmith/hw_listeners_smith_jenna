e06d95fe7065d6fda74d913f87aafcf172a45c28
Jenna-Smith <jennawsmith@outlook.com>
2017-07-13 14:16:30 -0400

    add eventlistener function

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ var lastName = document.getElementsByTagName('input')[1];
 var email = document.getElementsByTagName('input')[2];
 
 var message = document.getElementsByTagName('textarea')[0];
+
+document.addEventListener('submit', function (s) {
+
+    s.preventDefault();
+
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + email.value);
+    console.log('The message is: ' + message.value);
+});
