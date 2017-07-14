caf59af79293b7624c260d6a0f8ca50ecbfa38de
Jenna-Smith <jennawsmith@outlook.com>
2017-07-14 16:46:04 -0400

    adjust line spacing with js comments

--- js/main.js
+++ js/main.js
@@ -9,13 +9,14 @@ hover.addEventListener('mouseout', function (event) {
     event.target.style.color = '#6d7993';
 });
 
+
 // This listener will change the color of the background when the page loads
 window.addEventListener('load', function changeBackground() {
     document.body.style.backgroundColor = '#c28285';
 });
 
-//This listener will change the background-color of the sections when clicked
 
+//This listener will change the background-color of the sections when clicked
 document.getElementsByTagName('section')[0].addEventListener('click', function (event) {
     event.target.style.backgroundColor = '#d5d5d5';
 });
